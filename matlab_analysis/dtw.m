bd923f3da98db2a16a6aacd7343907fa40eedfd9
yh570 <yh570@nyu.edu>
2016-06-01 16:48:34 -0400

    add comments

--- matlab_analysis/dtw.m
+++ matlab_analysis/dtw.m
@@ -16,8 +16,8 @@ D(1,1)=0;
 %% Dynamic programming
 for i=1:ns
     for j=1:nt
-        d=abs(s(i)-t(j));
-        D(i+1,j+1)=d+min( [D(i,j+1), D(i+1,j), D(i,j)] );
+        d=abs(s(i)-t(j)); % get difference between each point
+        D(i+1,j+1)=d+min( [D(i,j+1), D(i+1,j), D(i,j)] ); % input the difference into the matrix
     end
 end
-d=D(ns+1,nt+1);
+d=D(ns+1,nt+1); % the total difference is the last element of the matrix
